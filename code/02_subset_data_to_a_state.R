dfcbff3ece5ef9fd45ccbb0efd527135ab38a743
Sarah Hingorani <svhingorani2@dons.usfca.edu>
2021-09-30 20:05:59 -0700

    Update this code file

--- code/02_subset_data_to_a_state.R
+++ code/02_subset_data_to_a_state.R
@@ -10,7 +10,10 @@
 # load functions
 source("code/functions/subset_mobility_data_to_state.R")
 
-# test out the use of the function?
-subset_mobility_data_to_state(
-  input_file_name = "data/raw_data/applemobilitytrends-2021-09-18.csv",
-  state_to_subset = "New York")
+# create a for loop to output files with given state names
+for (state in c("Texas", "Florida", "Maryland", "Illinois",
+                "Georgia", "Hawaii")) {
+  subset_mobility_data_to_state(
+    input_file_name = "data/raw_data/applemobilitytrends-2021-09-18.csv",
+    state_to_subset <- state)
+}
