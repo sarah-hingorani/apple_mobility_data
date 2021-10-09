f53f28fd6c6d80185e02eb3ec8f8ec64ae8ab699
Sarah Hingorani <svhingorani2@dons.usfca.edu>
2021-10-09 14:19:31 -0700

    Modify code and change commentary

--- code/03_count_up_counties_cities.R
+++ code/03_count_up_counties_cities.R
@@ -1,13 +1,30 @@
-# script goal: use the "dplyr" package to count up the number of cities and
-# counties in a given state that have detailed Apple mobility data available
-# and then eventually save it to a csv
+# script goal: write a csv file of the transportation tallies of a subsetted
+# state
 
 # Sarah Hingorani
 # September 26, 2021
 # svhingorani2@dons.usfca.edu
 
-# load the "dplyr" package
-library("dplyr")
+# load the function
+source("code/functions/subset_mobility_data_by_city_county_transportation.R")
+
+tally_state_of_choice <- c("New York",
+                            "Florida",
+                            "Maryland",
+                            "Illinois",
+                            "Georgia",
+                            "Hawaii",
+                            "Texas")
+
+# use a for loop to test the function
+for (tallied_state in tally_state_of_choice) {
+  state_of_choice <- tallied_state
+  print(paste("Tally transportation data for", tallied_state))
+}
+
+tally_state_of_choice <- subset_mobility_data_by_city_county_transportation(
+  input_file_name = paste0()
+)
 
 # we would like to get a count of the number of cities and the number of
 # counties in a given state that have driving mobility data. The input to this
@@ -15,15 +32,11 @@ library("dplyr")
 # were already subsetted.
 
 # load in the dataset from the previous script
-input_file_name <- paste0("output/subsetted_states/",
-                    "applemobilitytrends-2021-09-18_Texas.csv")
+input_file_name <- paste0("output/",
+                           "subsetted_states/",
+                           "applemobilitytrends-2021-09-18_New York.csv")
 state_data <- read.csv(input_file_name)
 
-# starting off with dplyr chains
-count_cities_counties_by_type <- state_data %>%
-  select(geo_type, region, transportation_type) %>%
-  group_by(geo_type, transportation_type) %>%
-  tally()
 
 
 # write out the result of the dplyr chain
