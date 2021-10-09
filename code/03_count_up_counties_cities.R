258f070ac0cf40a3ca49bf2e4bae08a0e1c7c82c
Sarah Hingorani <svhingorani2@dons.usfca.edu>
2021-10-09 15:45:31 -0700

    Delete tally_state_of_choice and extra spaces

--- code/03_count_up_counties_cities.R
+++ code/03_count_up_counties_cities.R
@@ -22,10 +22,6 @@ for (tallied_state in tally_state_of_choice) {
   print(paste("Tally transportation data for", tallied_state))
 }
 
-tally_state_of_choice <- subset_mobility_data_by_city_county_transportation(
-  input_file_name = paste0()
-)
-
 # we would like to get a count of the number of cities and the number of
 # counties in a given state that have driving mobility data. The input to this
 # will be the output of the previous script, namely the state csv files that
@@ -36,8 +32,6 @@ input_file_name <- paste0("output/",
                            "subsetted_states/",
                            "applemobilitytrends-2021-09-18_New York.csv")
 state_data <- read.csv(input_file_name)
-
-
 
 # write out the result of the dplyr chain
 write.csv(count_cities_counties_by_type,
