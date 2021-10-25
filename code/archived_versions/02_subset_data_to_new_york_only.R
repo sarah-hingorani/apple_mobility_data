f4b3163f9bee425ba6a884b67b3d30752c2a379f
Sarah Hingorani <svhingorani2@dons.usfca.edu>
2021-10-25 14:15:53 -0700

    Add a space between the comma and closing bracket

--- code/archived_versions/02_subset_data_to_new_york_only.R
+++ code/archived_versions/02_subset_data_to_new_york_only.R
@@ -12,7 +12,7 @@ all_covid_data <- read.csv("data/raw_data/applemobilitytrends-2021-09-05.csv")
 # subset the dataset to only include rows where the sub.region column has
 # "New York" in it but we want all columns.
 
-new_york_data <- all_covid_data[all_covid_data$sub.region == "New York",]
+new_york_data <- all_covid_data[all_covid_data$sub.region == "New York", ]
 
 # save the New York data to a new csv file in the output directory
 write.csv(new_york_data, file = "output/new_york_apple_mobility_data.csv")
