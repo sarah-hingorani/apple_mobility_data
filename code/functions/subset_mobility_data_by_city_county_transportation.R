08b00f46ed06fec6fee1235396891d81638051de
Sarah Hingorani <svhingorani2@dons.usfca.edu>
2021-10-24 21:17:40 -0700

    Update top entry comment

--- code/functions/subset_mobility_data_by_city_county_transportation.R
+++ code/functions/subset_mobility_data_by_city_county_transportation.R
@@ -1,6 +1,6 @@
 # Script to read in Apple mobility data from a csv file
-# and subset the data to just include rows from any particular US state
-# and then write out a csv file that only has that subset
+# and subset the data to just include rows from any particular US state, then
+# tally the transportation data type by city/county, and write it as a csv
 
 # Sarah Hingorani
 # svhingorani2@dons.usfca.edu
@@ -11,23 +11,26 @@
 # that is subsetted
 subset_mobility_data_to_state <- function(input_file_name,
                                           state_to_subset) {
+
   # read in the complete csv file
-  all_covid_data <- read.csv(input_file_name)
+  all_covid_data <- readr::read_csv(input_file_name)
 
   # subset the dataset to only include rows where the sub.region column has
   # the state name in it but we want all columns.
-  state_data <- all_covid_data[all_covid_data$sub.region == state_to_subset, ]
+  state_data <- all_covid_data %>%
+    dplyr::filter('sub.region' == state_to_subset)
 
   # check that the subsetted data actually has data in it
   if (nrow(state_data) == 0) {
-    stop("ERROR, no rows matching given state name. Did you make a typo?")
+    stop("ERROR, no rows matching given state name. Is there a typo?")
   }
 
   # save the state data to a new csv file in the output directory
-  write.csv(state_data, file =  paste0("output/",
+  readr::write_csv(state_data, file =  paste0("output/subsetted_states_wide/",
                                        tools::file_path_sans_ext(
                                          basename(input_file_name)),
                                        "_",
                                        state_to_subset,
                                        ".csv"))
+  return(state_data)
 }
