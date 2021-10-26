3ec94d4195f3422ecc9af9caf9b46d47d3bbc524
Sarah Hingorani <svhingorani2@dons.usfca.edu>
2021-10-25 19:43:33 -0700

    Change name from mobility... to subset_by_city_county

--- code/functions/subset_mobility_data_by_city_county_transportation.R
+++ code/functions/subset_mobility_data_by_city_county_transportation.R
@@ -12,7 +12,7 @@ library("dplyr")
 # create a function to subset any US state out of the full dataset
 # this should also create an output CSV file that is named based on the state
 # that is subsetted
-subset_mobility_data_to_state <- function(input_file_name, state_to_subset) {
+subset_by_city_county <- function(input_file_name, state_to_subset) {
 
   # read in the complete csv file
   all_covid_data <- readr::read_csv(input_file_name)
