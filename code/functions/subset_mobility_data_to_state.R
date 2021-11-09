73cffae363de922819ce93628cba5e73148c74f2
Sarah Hingorani <svhingorani2@dons.usfca.edu>
2021-11-08 16:13:05 -0800

    Add / to end of "output/subsetted_states_wide"

--- code/functions/subset_mobility_data_to_state.R
+++ code/functions/subset_mobility_data_to_state.R
@@ -36,7 +36,7 @@ subset_mobility_data_to_state <- function(input_file_name,
   }
 
    # save the state data to a new csv file in the output directory
-  readr::write_csv(state_subset, file = paste0("output/subsetted_states_wide",
+  readr::write_csv(state_subset, file = paste0("output/subsetted_states_wide/",
                                        tools::file_path_sans_ext(
                                        basename(input_file_name)),
                                       "_",
