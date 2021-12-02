289b500e6ce2f96e55544c6f6d697741deb8b883
Sarah Hingorani <svhingorani2@dons.usfca.edu>
2021-12-01 19:41:28 -0800

    Modify for loop code to create tallied data output for states selected

--- code/03_count_up_counties_cities.R
+++ code/03_count_up_counties_cities.R
@@ -8,26 +8,11 @@
 # load the function
 source("code/functions/subset_mobility_data_by_city_county_transportation.R")
 
-tally_state_of_choice <- c("New York",
-                            "Florida",
-                            "Maryland",
-                            "Illinois",
-                            "Georgia",
-                            "Hawaii",
-                            "Texas")
-
 # use a for loop to test the function
-for (tallied_state in tally_state_of_choice) {
-  state_of_choice <- tallied_state
-  print(paste("Tally transportation data for", tallied_state))
+
+for (state_to_subset in c("New York", "Florida", "Maryland", "Illinois",
+                          "Georgia", "Hawaii", "Texas")) {
+subset_by_city_county(input_file_name = paste0("data/raw_data/",
+                                        "applemobilitytrends-2021-09-18.csv"),
+                                        state_to_subset)
 }
-
-# we would like to get a count of the number of cities and the number of
-# counties in a given state that have driving mobility data. The input to this
-# will be the output of the previous script, namely the state csv files that
-# were already subsetted.
-
-# load in the dataset from the previous script
-input_file_name <- paste0("output/subsetted_states_wide/",
-                          "applemobilitytrends-2021-09-18_New_York.csv")
-state_data <- read.csv(input_file_name)
