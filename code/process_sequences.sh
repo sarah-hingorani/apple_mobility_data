062615e5cef870357201fb080a5dff05be4e6efc
Sarah Hingorani <svhingorani2@dons.usfca.edu>
2021-12-14 01:58:59 +0000

    Modify tally sequence code

--- code/process_sequences.sh
+++ code/process_sequences.sh
@@ -34,4 +34,4 @@ then
 fi
 
 # Tally the number of sequences, sorting them from largest to smallest
-zgrep "isolate.*Homo" "$1" | cut -d"|" -f5 | sort | uniq -c | sort -rn
+bioawk -c fastx '{print $comment}' "$1" | awk '{split($0,x,"|");print x[21]}' | sort | uniq -c |sort -rn
